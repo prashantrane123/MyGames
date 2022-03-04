a0edba114de1fa59b2b81e890807c212358b1763
Prashant_Rane <prashant.rane@husqvarnagroup.com>
2022-03-04 15:35:48 +0530

    Hilt setup done.

---
+++ app/src/main/java/com/example/mygames/MyApplication.kt
@@ -0,0 +1,12 @@
+package com.example.mygames
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by Prashant Rane on 04-03-2022.
+ */
+
+@HiltAndroidApp
+class MyApplication: Application() {
+}
